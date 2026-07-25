78a9992dc931c14125122d83f433ff6a4d9c8b2c
jarvis <abdullahplayz109@gmail.com>
2026-07-25 20:49:58 +0500

    added more operators and 0 protection in divison

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,5 +1,5 @@
 print("Welcome to Calculator!")
-operator = input("Choose an operator (+ - / *): ")
+operator = input("Choose an operator (+ - / * % //): ")
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
@@ -9,9 +9,17 @@ if operator == "+":
 elif operator == "-":
     ans = num1 - num2
     print(f"Your answer is {ans}")
-elif operator == "/":
+if num2 == 0:
+    print("Cannot divide by 0")
+elif operator == "/"  :
     ans = num1 / num2
     print(f"Your answer is {ans}")
-elif operator == "*":
+if operator == "*":
     ans = num1 * num2
     print(f"Your answer is {ans}")
+elif operator == "%":
+    ans = num1 % num2
+    print(f"Your answer is {ans}")
+elif operator == "//":
+    ans = num1 // num2
+    print(f"Your anser is {ans}")
