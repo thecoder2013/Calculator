a21b6514f133167a97188eac1954b4c1864f5241
jarvis <abdullahplayz109@gmail.com>
2026-07-26 15:32:01 +0500

    Fixed 0 protection in division, asks before exiting if the user wants to quit

--- Calculator/main.py
+++ Calculator/main.py
@@ -9,12 +9,13 @@ if operator == "+":
 elif operator == "-":
     ans = num1 - num2
     print(f"Your answer is {ans}")
-if num2 == 0:
-    print("Cannot divide by 0")
-elif operator == "/"  :
-    ans = num1 / num2
-    print(f"Your answer is {ans}")
-if operator == "*":
+elif operator == "/":
+    if num2 == 0:
+        print("Cannot divide by 0!")
+    else:
+        ans = num1 / num2
+        print(f"Your answer is {ans}")
+elif operator == "*":
     ans = num1 * num2
     print(f"Your answer is {ans}")
 elif operator == "%":
@@ -23,3 +24,37 @@ elif operator == "%":
 elif operator == "//":
     ans = num1 // num2
     print(f"Your anser is {ans}")
+else:
+    print("Choose a Valid Operator")
+finish = input("Do you want to quit? (Yes/No) ")
+while finish == "No":
+    operator = input("Choose an operator (+ - / * % //): ")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    if operator == "+":
+        ans = num1 + num2
+        print(f"Your answer is {ans}")
+    elif operator == "-":
+        ans = num1 - num2
+        print(f"Your answer is {ans}")
+    elif operator == "/":
+        if num2 == 0:
+            print("Cannot divide by 0!")
+        else:
+            ans = num1 / num2
+            print(f"Your answer is {ans}")
+    elif operator == "*":
+        ans = num1 * num2
+        print(f"Your answer is {ans}")
+    elif operator == "%":
+        ans = num1 % num2
+        print(f"Your answer is {ans}")
+    elif operator == "//":
+        ans = num1 // num2
+        print(f"Your anser is {ans}")
+    else:
+        print("Choose a Valid Operator")
+    again = input("Do you want to quit? (Yes/No) ")
+    if again == "Yes":
+        break
+print("Goodbye")
